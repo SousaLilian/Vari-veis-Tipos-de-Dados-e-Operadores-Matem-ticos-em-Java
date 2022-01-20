3998b0878576273a0c2d1e59c7f5fe521f8f7f77
Lilian de Sousa <s.lilian@hotmail.com>
2022-01-19 21:13:35 -0300

    Variáveis, Tipos de Dados e Operadores Matemáticos em Java.

---
+++ src/aula02/Main.java
@@ -0,0 +1,63 @@
+package aula02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		byte b1 = 20;
+		System.out.println(b1);
+		byte b2 = 20;
+		System.out.println(b2);
+		
+		short s1 = 20000;
+		// short s2 = 40000;
+		System.out.println(s1);
+		
+		//int i1 = 10000000000;
+		int i2 = 28500;
+		System.out.println(i2);
+		
+		long l1 = 1000000000000000000L;
+		System.out.println(l1);
+		
+		long l2 = 200400500500055000L;
+		System.out.println(l2);
+		
+		//float f1 = 4.5;
+		float f2 = 10.68F;
+		System.out.println(f2);
+		
+		double d1 = 85.69;
+		System.out.println(d1);
+		double d2 = 99.04D;
+		System.out.println(d2);
+		
+		char c1 = 'W';
+		System.out.println(c1);
+		
+		//char c2 = 'Tw";
+		char c3 = '\u0057';
+		System.out.println(c3);
+		
+		String st1 = "Fulano";
+		System.out.println(st1);
+		
+		String st2 = "Cicrano";
+		System.out.println(st2);
+		
+		String st3 = "ag dgb se se et t KNBJBJBB &*&%& 75894389";
+		System.out.println(st3);
+		
+		//String dt1 = "09/02/1981";
+		
+		boolean bo1 = true;
+		System.out.println(bo1);
+		
+		boolean bo2 = false;
+		System.out.println(bo2);
+		
+		
+		
+	}
+}
